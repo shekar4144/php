b3fc9d643ee40bd441b1b3aa8494ff6dc7714d5e
ankit20000 <37360428+ankit20000@users.noreply.github.com>
2019-07-09 18:48:15 +0530

    Update PHP_container.sh

--- PHP_container.sh
+++ PHP_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Start the docker service
-systemctl status docker 
-service docker restart
+#systemctl status docker 
+#service docker restart
 
 #Get the docker configuration file from git
 curl https://raw.githubusercontent.com/shubhindia/php_container/master/docker-compose.yml > docker-compose.yml
